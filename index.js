8141442247d08d8bfc68414971206fde617f0f2e
MaxIsWell42 <maxfin13@gmail.com>
2023-02-22 14:50:11 -0600

    [add] Date formatting, another route [fix] literal for the port

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Wow js so fun")
+const express = require('express')
+const app = express()
+const path = require("path")
+const format = require('date-format')
+const PORT = 4000 || process.env.PORT
+
+// Home
+app.get("/", (req,res) => {
+    res.status(200).send("<h1>OuterAct</h1>")
+})
+
+app.get("/api/v1/instagram", (req,res) => {
+    const instaSocial = {
+        username: 'MaxIsWell',
+        followers: 432,
+        following: 30,
+        date: format.asString('', new Date())
+    }
+
+    res.status(200).json(instaSocial)
+})
+
+app.get("/api/v1/linkedIn", (req,res) => {
+    const instaSocial = {
+        username: 'MaxIsWell',
+        connections: 70,
+        date: format.asString('', new Date())
+    }
+
+    res.status(200).json(instaSocial)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`)
+})
+
+
